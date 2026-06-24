411c4e677baa9bc603abf7fb3e79f6c14972fda6
levimarkmuigai <levimarkmuigai@gmail.com>
2026-06-24 11:47:02 +0300

    feat: icons

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import {
+  CloudUpload, CircleX, CirclePlus, Check, Loader, Download,
+  createIcons
+} from "lucide";
+
+createIcons({
+  icons: { CloudUpload, CircleX, CirclePlus, Check, Loader, Download }
+});
